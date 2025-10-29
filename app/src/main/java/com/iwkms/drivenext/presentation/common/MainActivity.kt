592ce607f042df0dc737493b9828b72e60ae041a
IWKMS99 <alievsaid0909@yandex.ru>
2025-10-29 23:42:55 +0300

    feat(ui): integrate BottomNavigationView into MainActivity

--- app/src/main/java/com/iwkms/drivenext/presentation/common/MainActivity.kt
+++ app/src/main/java/com/iwkms/drivenext/presentation/common/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.iwkms.drivenext.presentation.common
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.iwkms.drivenext.R
 import com.iwkms.drivenext.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
@@ -15,5 +21,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.splashScreenFragment,
+                R.id.onboardingFragment,
+                R.id.authFragment,
+                R.id.loginFragment,
+                R.id.registrationStep1Fragment,
+                R.id.registrationStep2Fragment,
+                R.id.registrationStep3Fragment,
+                R.id.registrationSuccessFragment -> {
+                    binding.bottomNavigation.visibility = View.GONE
+                }
+                else -> {
+                    binding.bottomNavigation.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 }
