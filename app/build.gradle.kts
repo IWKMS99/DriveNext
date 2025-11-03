6d7f44c33075e0b7528f89c801237599b8769671
IWKMS99 <alievsaid0909@yandex.ru>
2025-11-03 14:46:56 +0300

    chore(build): добавлена поддержка Safe Args для Navigation Component

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     id("org.jetbrains.kotlin.plugin.serialization")
+    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.7.7" apply false
 }
